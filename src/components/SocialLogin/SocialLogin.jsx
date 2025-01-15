b20e02e9329396f984db4e3f1b7754b6dec0bd88
ornobaadi <aadi4789@gmail.com>
2025-01-15 15:29:28 +0600

    No duplicate email with social

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,12 +1,26 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
 
 const SocialLogin = () => {
     const {googleSignIn} = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
+    const location = useLocation();
 
     const handleGoogleSignIn = () => {
         googleSignIn()
         .then(result => {
             console.log(result.user);
+            const userInfo = {
+                email: result.user?.email,
+                name: result.user?.displayName
+            }
+            axiosPublic.post('/users', userInfo)
+            .then(res => {
+                console.log(res.data);
+                navigate('/');
+            })
         })
     }
     return (
